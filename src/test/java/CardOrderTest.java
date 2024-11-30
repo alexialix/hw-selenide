1a542ce257aa88ddd0b708250594c5724c487f8d
Alexia <trup.lexa@gmail.com>
2024-11-30 19:12:33 +0500

    fix #2

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,28 +1,20 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-class CardOrderTest {
-
-
-    @BeforeEach
-    void setUp() {
-        Configuration.baseUrl = "http://localhost:9999";
-    }
+public class CardOrderTest {
 
     @Test
     void shouldSubmitFormSuccessfully() {
         open("http://localhost:9999");
-
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("+79012345678");
-        $("[data-test-id=agreement]").click();
-        $("button[type=button]").click();
-        $("[data-test-id=order-success]")
-                .shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иван");
+        form.$("[data-test-id=phone] input").setValue("+79112345678");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
-
