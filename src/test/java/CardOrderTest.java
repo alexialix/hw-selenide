9fb2e46f5c866c8b37d25372f825e2018b052f83
Alexia <trup.lexa@gmail.com>
2024-11-30 20:42:21 +0500

    appveyor.yml updated

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,20 +1,20 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CardOrderTest {
-
+class Card {
     @Test
-    void shouldSubmitFormSuccessfully() {
+    void applicationMustPass() throws InterruptedException {
         open("http://localhost:9999");
+
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("Иван");
-        form.$("[data-test-id=phone] input").setValue("+79112345678");
+        form.$("[data-test-id=name] input").setValue("Иван Иванов");
+        form.$("[data-test-id=phone] input").setValue("+79012345678");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
