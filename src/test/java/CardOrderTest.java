a596c8b1e3c398bed5b5c7aac39f407227de65fd
Alexia <trup.lexa@gmail.com>
2024-12-01 11:33:53 +0500

    updated

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -5,16 +5,29 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class Card {
+class CardOrderTest  {
     @Test
-    void applicationMustPass() throws InterruptedException {
+    void shouldSubmitFormSuccessfully() {
         open("http://localhost:9999");
 
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("Иван Иванов");
-        form.$("[data-test-id=phone] input").setValue("+79012345678");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+    @Test
+    void shouldErrorWhenInvalidName() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Ivan Ivanov");
+        $("[data-test-id=phone] input").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
+
+
